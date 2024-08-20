50c0fca1955e2c0167ce508c541cd82ca31eb953
André Oliveira <74216375+andrefc1995@users.noreply.github.com>
2024-08-20 18:37:49 -0300

    Add files via upload

---
+++ decode_morse.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import datetime
+import pandas as pd
+from config import file_path, dict_morse
+
+def decode_morse(msg):
+    '''
+    input : mensagem em código morse com as letras separadas por espaços e as palavras por dois espaços
+    output : mensagem em texto claro
+    '''
+    msg_palavras = msg.split("  ")
+    msg_claro = []
+    for palavra in msg_palavras:
+        msg_letras = palavra.split(" ")
+        palavra_claro = "".join([dict_morse[letter] for letter in msg_letras])
+        msg_claro.append(palavra_claro)
+    return " ".join(msg_claro)
+
+def save_clear_msg_csv_hdr(msg_claro):
+    '''
+    input : mensagem em texto claro
+    output : mensagem escrita em letras e algarismos, salva em arquivo csv
+    '''
+    now = datetime.datetime.now()
+    df = pd.DataFrame([[msg_claro, now]], columns=["mensagem", "datetime"])
+    hdr = not os.path.exists(file_path)
+    df.to_csv(file_path, mode ="a", index = False, header=hdr)
+
+if __name__ == "__main__":
+    msg_claro = decode_morse(sys.argv[1])
+    save_clear_msg_csv_hdr(msg_claro)
